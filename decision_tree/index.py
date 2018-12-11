cf24238e555dd0d8e89080784cf20827a98a1b1b
katherine0325 <510175889@qq.com>
2018-12-11 23:57:05 +0800

    desision tree

--- decision_tree/index.py
+++ decision_tree/index.py
@@ -1,13 +1,12 @@
-from sklearn.feature_extration import DictVectorizer
+from sklearn.feature_extraction import DictVectorizer
 import csv
 from sklearn import tree
 from sklearn import preprocessing
 from sklearn.externals.six import StringIO
 
-with open('sdfsdfsdf.csv', 'rb') as f:
+with open('decision_tree/data_file.csv', 'rt') as f:
     reader = csv.reader(f)
-    headers = reader.next()
-    print headers
+    headers = next(reader)
     featureList = []
     labelList = []
 
@@ -18,4 +17,33 @@ with open('sdfsdfsdf.csv', 'rb') as f:
             rowDict[headers[i]] = row[i]
         featureList.append(rowDict)
 
-    print featureList
+# 将特征变为 sklearn 可输入的形式 -- 向量
+vec = DictVectorizer()
+dummyX = vec.fit_transform(featureList).toarray()
+
+# sklearn 有专门将二值标签向量化的函数
+lb = preprocessing.LabelBinarizer()
+dummyY = lb.fit_transform(labelList)
+
+# 决策树算法
+clf = tree.DecisionTreeClassifier(criterion='entropy')
+clf = clf.fit(dummyX, dummyY)
+
+# Visualize model
+with open('allElectronicInformationGainOri.dot', 'w') as f:
+    f = tree.export_graphviz(clf, feature_names=vec.get_feature_names(), out_file=f)
+
+# 进行预测，将第一个样本的第一个属性改变
+# 改变前
+oneRowX = dummyX[0, :]
+print(str(oneRowX))
+
+# 改变后
+newRowX = oneRowX
+newRowX[0] = 1
+newRowX[2] = 0
+print(str(newRowX))
+
+#预测
+predictedY = clf.predict([newRowX])
+print("predictedY: " + str(predictedY))
